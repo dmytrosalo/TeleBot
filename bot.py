592aab83ca0f089ba1a2402b9729dd6248ae2653
dmitriysalo <d.salo@atomcream.com>
2018-07-02 17:30:44 +0300

    change proc file

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text + " ХУЙ")
+    bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
