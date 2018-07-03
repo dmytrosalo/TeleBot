72e6d64699ff6e49e6e290f139e62f3db80c94b9
dmitriysalo <d.salo@atomcream.com>
2018-07-03 18:41:41 +0300

    add buttons

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 import config
 
 import telebot
-
+from telebot import types
 
 bot = telebot.TeleBot(config.token)
 
+source_markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+source_markup_btn1 = types.KeyboardButton('Ongoing')
+source_markup_btn2 = types.KeyboardButton('Upcoming')
+source_markup.add(source_markup_btn1, source_markup_btn2)
 
 @bot.message_handler(commands=['start', 'go'])
 def start_handler(message):
